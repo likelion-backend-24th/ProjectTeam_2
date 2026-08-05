2d51ee88d7a9767ff5296a5956ba625388c84061
ChoiSeungHwan <csh980930@naver.com>
2026-08-05 13:37:57 +0900

    feat: 내 정보 조회 서비스 로직 추가, 응답 DTO추가, 예외처리코드 추가

---
+++ backend/src/main/java/org/example/backend/user/exception/UserErrorCode.java
@@ -0,0 +1,16 @@
+package org.example.backend.user.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.backend.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
--- backend/src/main/java/org/example/backend/user/service/UserService.java
+++ backend/src/main/java/org/example/backend/user/service/UserService.java
@@ -1,5 +1,33 @@
 package org.example.backend.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.backend.common.exception.BusinessException;
+import org.example.backend.user.dto.UserResponse;
+import org.example.backend.user.entity.User;
+import org.example.backend.user.exception.UserErrorCode;
+import org.example.backend.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
-    //커밋용
+    private final UserRepository userRepository;
+
+    public UserResponse getMyInfo(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .name(user.getName())
+                .nickname(user.getNickname())
+                .role(user.getRole())
+                .status(user.getStatus())
+                .subscribed(user.isSubscribed())
+                .build();
+
+    }
+
 }
