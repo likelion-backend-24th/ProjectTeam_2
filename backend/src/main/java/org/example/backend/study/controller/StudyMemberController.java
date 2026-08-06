341c448243dbdfd6bb835d99cc0e652ff6e05c20
milk0621 <wltjs5360@gmail.com>
2026-08-06 21:19:35 +0900

    feat: 스터디 멤버 관리 기능 추가

---
+++ backend/src/main/java/org/example/backend/study/controller/StudyMemberController.java
@@ -0,0 +1,4 @@
+package org.example.backend.study.controller;
+
+public class StudyMemberController {
+}
---
+++ backend/src/main/java/org/example/backend/study/service/StudyMemberService.java
@@ -0,0 +1,4 @@
+package org.example.backend.study.service;
+
+public class StudyMemberService {
+}
