4827dd84e80898d56a3dabac1343e019a6df3804
ChoiSeungHwan <csh980930@naver.com>
2026-08-06 17:02:20 +0900

    fix: 필요없는 코드인데 빼버림

--- backend/src/main/java/org/example/backend/auth/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/auth/config/SecurityConfig.java
@@ -42,7 +42,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/logout").authenticated() //로그아웃 인증!!
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/auth/me").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
