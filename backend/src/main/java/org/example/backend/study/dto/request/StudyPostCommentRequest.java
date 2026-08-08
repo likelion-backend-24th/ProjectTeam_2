c83b9b5943f3fc7352481862a87f62fe64dd96be
ChoiSeungHwan <csh980930@naver.com>
2026-08-08 21:44:23 +0900

    docs: description 수정

--- backend/src/main/java/org/example/backend/study/dto/request/StudyPostCommentRequest.java
+++ backend/src/main/java/org/example/backend/study/dto/request/StudyPostCommentRequest.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public class StudyPostCommentRequest {
 
-    @Schema(description = "댓글 내용", example = "지각 안하겠습니다!")
+    @Schema(description = "게시글 댓글 내용", example = "지각 안하겠습니다!")
     @NotBlank(message = "댓글 내용을 입력해주세요.")
     @Size(max = 1000, message = "댓글은 1000자를 초과할 수 없습니다.")
     private String content;
--- backend/src/main/java/org/example/backend/study/dto/request/StudyPostCommentUpdateRequest.java
+++ backend/src/main/java/org/example/backend/study/dto/request/StudyPostCommentUpdateRequest.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public class StudyPostCommentUpdateRequest {
 
-    @Schema(description = "수정할 댓글 내용", example = "수요일로 알겠습니다!")
+    @Schema(description = "수정할 게시글 댓글 내용", example = "수요일로 알겠습니다!")
     @NotBlank(message = "댓글 내용을 입력해주세요.")
     @Size(max = 1000, message = "댓글은 1000자를 초과할 수 없습니다.")
     private String content;
